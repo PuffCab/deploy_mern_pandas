6f6ccaae655121f2f4d7a507121551d08a8e96f7
PuffCab <berlinkollner@gmail.com>
2024-07-09 12:56:18 +0200

    base url ready for production mode

--- client/src/utils/baseUrl.ts
+++ client/src/utils/baseUrl.ts
@@ -1,2 +1,8 @@
 // export const baseUrl = "http://localhost:5002";
-export const baseUrl = "https://deploy-mern-pandas-server.vercel.app";
+// export const baseUrl = "https://deploy-mern-pandas-server.vercel.app";
+
+// export const baseUrl = "http://localhost:5002";
+export const baseUrl =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5002"
+    : "https://deploy-mern-pandas-server.vercel.app";
